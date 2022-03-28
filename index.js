0f93cb7788460e6e0fb7d89f76aaa98bc486f523
Abu Hurayra <hurayraiit@gmail.com>
2022-03-28 07:42:23 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 "use strict";
 
 const {Builder, By, Key, until} = require('selenium-webdriver');
-const config = require('./config.json');
+const config = require('./util/config.json');
 
 async function example() {
     let driver = await new Builder().forBrowser('firefox').build();
     await driver.get(`${config.site_root}/docs`);
+    await driver.findElement(By.name('s')).sendKeys('test');
+    await driver.sleep(3000);
     
 }
 
