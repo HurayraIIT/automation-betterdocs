aa354b1718b0dfa4229aabf81bcc339f81d022a2
Abu Hurayra <hurayraiit@gmail.com>
2022-07-29 11:22:12 +0600

    initial project files

---
+++ Tests/test_login.py
@@ -0,0 +1,17 @@
+import time
+from Pages.loginPage import LoginPage
+from Pages.TradingCompany.dashboardPage import DashboardPage
+from config import LINKS
+
+
+def test_login_logout_valid(browser):
+    pass
+    # login = LoginPage(browser)
+    # login.load()
+    # login.login(LINKS.TRADING_EMAIL, LINKS.PASSWORD)
+    # time.sleep(2)
+    #
+    # dashboard = DashboardPage(browser)
+    # dashboard.load()
+    # dashboard.click_logout()
+    # time.sleep(5)
---
+++ config.py
@@ -0,0 +1,23 @@
+
+class LINKS:
+
+    ROOT = "bd-automation.local"
+
+    ADMIN_USERNAME = "hurayra"
+    ADMIN_EMAIL = "wpdabh+admin@gmail.com"
+    PASSWORD = "pass1234"
+
+    # Which site do you want to test?
+    SITE = ROOT
+
+    # Common Pages
+    LOGIN_PAGE = SITE + ""
+    REGISTER_PAGE = SITE + ""
+    FORGOT_PASSWORD_PAGE = SITE + ""
+
+    # Pages
+
+
+class DATA:
+    # Test Data
+    pass
